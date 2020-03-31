f0ba9029031264b14d4891633133cedd0d026be1
Karolayn <karolaynmazo@gmail.com>
2020-03-31 15:27:51 -0500

    se creo la vista del registro

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+var btnRegistro=document.getElementById("btn_registrar");
+console.log(btnRegistro);
+
+function irARegistro(){
+    window.location.href="./registro.html";
+}
+
+btnRegistro.addEventListener("click",irARegistro);
+
+
