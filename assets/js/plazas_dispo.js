859127d94ea09ba8ad518a6deb58ff7e0b017fbb
KarolaynMazo <karolaynmazo@gmail.com>
2020-05-01 16:35:42 -0500

    se hacen las plazas disponibles

--- assets/js/plazas_dispo.js
+++ assets/js/plazas_dispo.js
@@ -4,4 +4,26 @@ $(document).ready(function(){
     if(permisos == null){
         window.location.href="./index.html";
     }
+    $.ajax({
+        type: 'POST',
+        //dataType: 'json', //Opcional
+        //contentType: 'application/json', //Opcional
+        url: 'http://localhost/poliparqueo_back/servicios/registros.php',
+        data:"metodo=consultarPlazasLibres&parqueadero="+localStorage.getItem("parqueadero_asignado"),
+        success: function(response){
+            if(response.result == true){
+                $("#plazas_libres").val(response.libres);
+            
+            }else{
+                console.log(response);
+            }
+            console.log(response);
+        },
+        error: function(response){
+            console.log(response);
+        },
+        timeout: 2000 //Opcional
+    });
+
+
 });
